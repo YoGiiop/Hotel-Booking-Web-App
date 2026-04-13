d495ceddef91fb5618cbc00feaa12c20f79b50fc
Yogesh <yogeshbhakuni30@gmail.com>
2026-04-13 09:50:35 +0530

    Updated files

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -68,8 +68,8 @@ const Dashboard = () => {
                         {
                             dashboardData.bookings.map((item, index) => (
                                 <tr key={index}>
-                                    <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>{item.user.username}</td>
-                                    <td className='py-3 px-4 text-gray-400 border-t border-gray-300 max-sm:hidden'>{item.room.roomType}</td>
+                                    <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>{item.user?.username || 'Unknown User'}</td>
+                                    <td className='py-3 px-4 text-gray-400 border-t border-gray-300 max-sm:hidden'>{item.room?.roomType || 'N/A'}</td>
                                     <td className='py-3 px-4 text-gray-400 border-t border-gray-300 text-center'>{currency} {item.totalPrice}</td>
                                     <td className='py-3 px-4  border-t border-gray-300 flex'>
                                         <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? "bg-green-200 text-green-600" : "bg-amber-200 text-yellow-600"}`}>
